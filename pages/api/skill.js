3ad601ac852fa484cfac2c9da7ed7a99e6c272b1
xct007 <hackerxct007@gmail.com>
2022-07-08 21:01:55 +0700

    Gaya

---
+++ pages/api/skill.js
@@ -0,0 +1,17 @@
+const Learning = [
+    "JavaScript.",
+    "ReactJs.",
+    "NextJs.",
+    "Python."
+]
+const Languages = [
+    {
+        language: "Indonesia",
+        level: "Professional",
+    },
+    {
+        language: "English",
+        level: "Intermediate"
+    }
+]
+export default Learning
